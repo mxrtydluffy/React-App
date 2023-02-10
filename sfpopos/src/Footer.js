711e832a94548d781ddb1537d983aead33807cb4
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-09 17:32:54 -0800

    dynamic content and footer

---
+++ sfpopos/src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='Footer'>
+            <p>Marty Marinda copyright 2023</p>
+        </div>
+    )
+}
+
+export default Footer;
