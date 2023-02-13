e43d5c84746c77dfa69cc45425254e3115c6f20f
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-13 15:28:13 -0800

    [UPDATE] files & made changes

---
+++ sfpopos/src/components/POPOSDetails/POPOSInfo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import './POPOSDetails.css'
+
+function POPOSDetails(props) {
+    const params = useParams()
+    const { id } = params
+    const { images, title, desc, hours, features, geo } = data[id]
+
+    return (
+        <div className='POPOSDetails'>
+          <div className='POPOSDetails-image'>
+            <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title} />
+          </div>
+          <div className='POPOSDetails-info'>
+            <h1 className='POPOSDetails-title'>{ title }</h1>
+            <p className='POPOSDDetails-desc'>{ desc }</p>
+            <p className='POPOSDetails-hours'>{ hours }</p>
+            <POPOSFeatureList features={features} />
+            <p className='POPOSDetails-geo'>{ geo.lat } { geo.lon }</p>
+          </div>
+    
+        </div>
+    )
+}
+
+export default POPOSDetails
