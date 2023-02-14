e3d9c1e46d5d27bdee216f2c8248e45f9ac31a57
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-13 17:33:37 -0800

    [UPDATE] POPOSSpace

---
+++ sfpopos/src/components/POPOSSpace/POPOSSpace.js
@@ -0,0 +1,24 @@
+import './POPOSSpace.css'
+import { Link } from 'react-router-dom'
+
+function POPOSSpace(props) {
+  const { name, image, address, hours, id } = props
+  return (
+    <div className='POPOSSpace'>
+      <h1>
+        <Link to={`/details/${id}`} className='POPOSSpace-title'>
+          {name}
+        </Link>
+      </h1>
+      <Link to={`/details/${id}`}>
+        <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt="Hello"/>
+      </Link>
+      <div className='POPOSSpace-info'>
+        <div>{address}</div>
+        <div className='hours'>{hours}</div>
+      </div>
+    </div>
+  )
+}
+
+export default POPOSSpace
